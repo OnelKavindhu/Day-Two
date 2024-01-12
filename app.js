558c25b406009e5116acc65fe4a9ffaab0d1000d
OnelKavindhu <106512102+OnelKavindhu@users.noreply.github.com>
2024-01-12 23:26:49 +0530

    2024.01.12  commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //console.table  ({name:"Onel", age:22})//this is how we write a table
 
+/*challange 1
 let name = "Onel";
 let whatDoYouWannaBecomeInYourLife = "Programmer";
 let gender = "Male";
@@ -9,3 +10,22 @@ console.log(name);
 console.log(whatDoYouWannaBecomeInYourLife);
 console.log(gender);
 console.log(xHandle);
+*/
+
+/*Challange 2
+let firstFavNumber = 33;
+let seconfFavNumber = 47;
+console.log(firstFavNumber + seconfFavNumber);
+console.log(firstFavNumber - seconfFavNumber);
+console.log(firstFavNumber * seconfFavNumber);
+console.log(firstFavNumber  /seconfFavNumber);
+console.log(firstFavNumber % seconfFavNumber);
+console.log(firstFavNumber ** seconfFavNumber);
+*/
+
+//Challange 3
+let isJSaProgrammingLanguage = true;
+let isItHard = false;
+let favNum = 33;
+
+console.log(favNum + undefined);
