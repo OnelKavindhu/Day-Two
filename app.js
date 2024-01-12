7fa110e603842e34e9f83afb7c1c8da33a05ca25
OnelKavindhu <106512102+OnelKavindhu@users.noreply.github.com>
2024-01-12 22:53:39 +0530

    2024.01.12  commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.table  ({name:"Onel", age:22})
+//console.table  ({name:"Onel", age:22})//this is how we write a table
+
+let name = "Onel";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Male";
+let xHandle = "@KaviOnel";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(xHandle);
