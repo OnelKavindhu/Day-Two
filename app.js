97adaaaa5d5573deee93c5d573d0f7afd5510c24
OnelKavindhu <106512102+OnelKavindhu@users.noreply.github.com>
2024-01-20 17:23:30 +0530

    commit 2023.01.20- 5.23.pm

--- app.js
+++ app.js
@@ -23,9 +23,35 @@ console.log(firstFavNumber % seconfFavNumber);
 console.log(firstFavNumber ** seconfFavNumber);
 */
 
-//Challange 3
+/*Challange 3
 let isJSaProgrammingLanguage = true;
 let isItHard = false;
 let favNum = 33;
 
+console.log(isJSaProgrammingLanguage);
+console.log(isItHard);
 console.log(favNum + undefined);
+*/
+
+//comparison operators 
+
+/*let firstFavNumber = 33;
+let seconfFavNumber = 3;
+ 
+console.log(firstFavNumber > seconfFavNumber);
+console.log(firstFavNumber < seconfFavNumber);
+console.log(firstFavNumber >= seconfFavNumber);
+console.log(firstFavNumber <= seconfFavNumber);
+
+console.log(firstFavNumber === seconfFavNumber);
+console.log(firstFavNumber !== seconfFavNumber);
+console.log(firstFavNumber == seconfFavNumber);
+console.log(firstFavNumber != seconfFavNumber);*/
+
+
+let firstName = 'Onel';
+let secondName = "Kavindhu";
+let fullName = firstName+' '+secondName;
+
+console.log(fullName.slice(0, 1));
+
